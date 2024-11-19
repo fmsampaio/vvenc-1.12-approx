7cb6010c30980e51b91fec310eefba4bd2082cec
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-11-19 14:41:45 -0300

    [feat] Macros for dyn approx

--- source/Lib/CommonLib/ApproxHandler.h
+++ source/Lib/CommonLib/ApproxHandler.h
@@ -5,18 +5,25 @@
 
 namespace vvenc {
 
-#define ENABLE_ORIG_SB_APPROX 1
-#define ENABLE_NEIGH_SB_APPROX 1
+#define SRAM_ECC_APPROX_LOW_CONFIG              1            // SRAM 0.7V  ECC
+#define SRAM_ECC_APPROX_MEDIUM_CONFIG           2            // SRAM 0.6V  ECC
+#define SRAM_ECC_APPROX_HIGH_CONFIG             3            // SRAM 0.55V ECC
 
-#define ORIG_SB_CONFIG 1
-#define ORIG_SB_BUFFER_Y 1
-#define ORIG_SB_BUFFER_CB 2
-#define ORIG_SB_BUFFER_CR 3
+#define ENABLE_DYNAMIC_APPROX                   1            // 0 - Static; 1 - Dynamic
 
-#define NEIGH_SB_CONFIG 2
-#define NEIGH_SB_BUFFER_Y 4
-#define NEIGH_SB_BUFFER_CB 5
-#define NEIGH_SB_BUFFER_CR 6
+#define ENABLE_ORIG_SB_APPROX                   1         
+#define ENABLE_NEIGH_SB_APPROX                  1
+
+#define ORIG_SB_CONFIG                          1            // Static Approximation
+#define ORIG_SB_BUFFER_Y                        1
+#define ORIG_SB_BUFFER_CB                       2
+#define ORIG_SB_BUFFER_CR                       3
+
+#define NEIGH_SB_CONFIG                         2            // Static Approximation
+#define NEIGH_SB_BUFFER_Y                       4
+#define NEIGH_SB_BUFFER_CB                      5
+#define NEIGH_SB_BUFFER_CR                      6
+
 
 class ApproxHandler {
     public:
